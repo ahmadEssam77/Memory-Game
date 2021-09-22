d52eee53e268121280239d395942426619d92632
ahmadEssam77 <ahmad77essam@gmail.com>
2021-09-22 19:59:41 +0200

    first try to build the game

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*
+###Tasks
+1- Knowing where you clicked on the page
+2- Hide the img-cover the one that has clicked and get the src of the img that was behined that cover
+3- Match the two sorces after the second click, if the src is matched ==> score function
+                                                           not matched ==> Failed Attempts function
+                                                           Then make the img-cover show over the img game again.
+4- Adjust how many attempts should be used.
+5- Repeat some functionality 
+6- Show congratulation message if he completed the game 
+
+*/
+
+// =============== Select the needed elements ===============
+// const coverImg = document.querySelectorAll(".cover-img");
+
+// for (let i = 0; i < coverImg.length; i++) {
+//     coverImg[i].addEventListener("click", (e)=> {
+//         e.target.style.opacity = "0";
+//     });
+// }
+
